8a2ce2b206557f2d4cd3b3f98497dec2b970a20d
Tal Levenberg <t0547990799@gmail.com>
2025-09-02 20:31:31 +0300

    cleanup: remove temp SW cache killer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,4 @@
 // src/main.jsx
-if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(regs => regs.forEach(r => r.unregister()));
-  if (window.caches && caches.keys) {
-    caches.keys().then(keys => keys.forEach(k => caches.delete(k)));
-  }
-}
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
